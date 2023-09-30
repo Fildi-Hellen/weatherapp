286a51e59725180348cec3a9accd7ad9d667624f
Fildi-Hellen <intysilva1@gmail.com>
2023-09-30 17:21:16 +0100

    created a react weather app with fake data

---
+++ src/Navigation.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+ import ReactDOM from 'react-dom/client'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import "./Navigation.css";
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <header>
+        <ul className="navigation-items">
+          <li className="navigation-item">
+            <Link to="/">Lisbon</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="/paris">Paris</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="/sydney">Sydney</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="/san-francisco">San Francisco</Link>
+          </li>
+        </ul>
+      </header>
+    );
+  }
+}
